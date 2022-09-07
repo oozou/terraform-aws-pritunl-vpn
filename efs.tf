aa741f074dac97a9d2bb46945f3816d4548f5d07
sedthawut tipkanpirome <sedthawut2000@gmail.com>
2022-09-07 15:25:36 +0700

    (update): efs version

--- efs.tf
+++ efs.tf
@@ -1,10 +1,11 @@
 # EFS
 module "efs" {
-  source = "git::ssh://git@github.com/oozou/terraform-aws-efs.git?ref=v1.0.1"
+  source = "git::ssh://git@github.com/oozou/terraform-aws-efs.git?ref=v1.0.3"
 
   prefix                    = var.prefix
   environment               = var.environment
   name                      = "pritunl-data"
+  encrypted                 = true
   enabled_backup            = var.enabled_backup
   efs_backup_policy_enabled = var.efs_backup_policy_enabled
   access_points = {
@@ -23,4 +24,6 @@ module "efs" {
   }
   vpc_id  = var.vpc_id
   subnets = var.private_subnet_ids
+
+  additional_efs_resource_policies = []
 }
--- examples/complete/vpc.tf
+++ examples/complete/vpc.tf
@@ -1,16 +1,22 @@
 module "vpc" {
-  source                       = "git@github.com:oozou/terraform-aws-vpc.git?ref=v1.1.6"
-  prefix                       = var.prefix
-  environment                  = var.environment
-  cidr                         = "10.105.0.0/16"
-  private_subnets              = ["10.105.60.0/22", "10.105.64.0/22", "10.105.68.0/22"]
-  public_subnets               = ["10.105.0.0/24", "10.105.1.0/24", "10.105.2.0/24"]
-  database_subnets             = ["10.105.20.0/23", "10.105.22.0/23", "10.105.24.0/23"]
-  availability_zone            = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
-  is_enable_dns_hostnames      = true
-  is_enable_dns_support        = true
-  is_create_nat_gateway        = true
-  is_enable_single_nat_gateway = true
-  account_mode                 = "hub"
-  tags                         = var.tags
+  source = "git@github.com:oozou/terraform-aws-vpc.git?ref=v1.1.7"
+
+  prefix       = var.prefix
+  environment  = var.environment
+  account_mode = "spoke"
+
+  cidr              = "172.17.170.128/25"
+  public_subnets    = ["172.17.170.192/28", "172.17.170.208/28"]
+  private_subnets   = ["172.17.170.224/28", "172.17.170.240/28"]
+  database_subnets  = ["172.17.170.128/27", "172.17.170.160/27"]
+  availability_zone = ["ap-southeast-1b", "ap-southeast-1c"]
+
+  is_create_nat_gateway             = true
+  is_enable_single_nat_gateway      = true
+  is_enable_dns_hostnames           = true
+  is_enable_dns_support             = true
+  is_create_flow_log                = false
+  is_enable_flow_log_s3_integration = false
+
+  tags = var.tags
 }
