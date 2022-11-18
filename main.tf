f2c2a7a2c17d089f4be21d2afa633331cc38ada5
be99inner <anurak.jann@gmail.com>
2022-11-18 14:45:48 +0700

    fix: fix ami for pritunl

--- main.tf
+++ main.tf
@@ -49,16 +49,18 @@ data "aws_vpc" "this" {
 # #############################################################################
 data "aws_ami" "amazon_linux" {
   most_recent = true
+
   filter {
     name   = "name"
-    values = ["amzn2-ami-*-hvm-*-x86_64-ebs"]
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-  owners = ["137112412989"] # amazon
+
+  owners = ["amazon"]
 }
 
 # #############################################################################
