f7c69ac25f7c1929bea1ebe840ad100244e8c169
be99inner <anurak.jann@gmail.com>
2022-11-18 15:29:58 +0700

    fix: fix ami for pritunl

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ data "aws_ami" "amazon_linux" {
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    values = ["amzn2-ami-*-gp2"]
   }
 
   filter {
@@ -62,6 +62,11 @@ data "aws_ami" "amazon_linux" {
 
   filter {
     name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "block-device-mapping.volume-type"
     values = ["gp2"]
   }
 
